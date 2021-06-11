a2cb612f59c34c3d6638f35542705aab4b4e74a1
Katerina Frid <catherinefrid96@gmail.com>
2021-06-11 10:10:51 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {connect} from "react-redux";
+import {Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from "./components/About";
+import Try from "./components/Try";
+import i18n from "./i18n";
 
-function App() {
+
+import {createGlobalStyle} from "styled-components";
+import PressStartTTF from "./fonts/PressStart2P-Regular.ttf";
+import SourceCodeProMedium from "./fonts/SourceCodePro-Medium.ttf";
+import styled from 'styled-components'
+
+
+
+
+const lang = i18n.language;
+const changeLanguage = lng => {
+  i18n.changeLanguage(lng);
+};
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0 0 0 calc(100vw - 100%);
+    padding: 0;
+    height: 100%;
+    font-family: 'Roboto Regular', monospace;
+    overflow: auto;
+  }
+  @font-face {
+    font-family: 'Press Start 2P';
+    src: url(${PressStartTTF}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Source Code Pro Medium';
+    src: url(${SourceCodeProMedium}) format('truetype');
+    font-style: normal;
+  }
+`;
+
+
+export const Container = styled.div`{
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}`
+
+export const Content = styled.div`{
+   
+  position: relative;
+  width: 100%;
+  height:100%;
+  display: flex;
+  flex:1;
+}`
+
+
+
+let App = ({ match, location }) => {
+  console.log("location", location);
+  console.log("match", match);
+
+
+  if (lang !== match.params.locale) {
+    changeLanguage(match.params.locale);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Container>
+          <GlobalStyle/>
+
+          <Navbar match={match} location={location}/>
+            <Content>
+              <Switch>
+                <Route path={`${match.url}/about`} component={() => <About match={match} />} />
+                <Route path={`${match.url}/`} component={() => <Try match={match} /> } />
+              </Switch>
+            </Content>
+            <Footer/>
+        </Container>
+    </>
   );
-}
+};
+
+App = connect(
+  state => ({
+    location: state.routing.location
+  }),
+  null
+)(App);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { ConnectedRouter, routerReducer } from "react-router-redux";
+import { createBrowserHistory } from "history";
+import { I18nextProvider } from "react-i18next";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+import i18n from "./i18n";
+
+import reducer from "./reducer";
+
+import App from './App';
+
+require('dotenv').config();
+
+const store = createStore(
+  combineReducers({
+    app: reducer,
+    routing: routerReducer
+  })
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const history = createBrowserHistory();
+
+const BasicExample = ({ store, history }) => (
+    <I18nextProvider i18n={i18n}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+            {/*<Route path="/:locale" component={Navbar} />*/}
+            <Route path="/:locale" component={App} />
+
+          <Redirect to="/en" />
+        </Switch>
+      </ConnectedRouter>
+    </Provider>
+  </I18nextProvider>
+
+
+);
+
+render(<BasicExample store={store} history={history} />, document.getElementById('root'));
